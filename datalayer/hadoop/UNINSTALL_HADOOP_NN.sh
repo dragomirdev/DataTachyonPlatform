8807c3570ed889e97b1e82bcae3cc4fe473e5e96
Silo-Six <32763958+Silo-Six@users.noreply.github.com>
2019-09-06 14:47:49 +0100

    initial scripts

---
+++ datalayer/hadoop/UNINSTALL_HADOOP_NN.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+HADOOP_SNN_IP=$1
+HADOOP_SNN_NAME=$2
+
+HADOOP_DN_ONE_IP=$3
+HADOOP_DN_ONE_NAME=$4
+
+HADOOP_DN_TWO_IP=$5
+HADOOP_DN_TWO_NAME=$6
+
+cd
+
+# Remove 2nd name node and data node vm's from hosts
+sudo sed -i "/$HADOOP_SNN_IP $HADOOP_SNN_NAME/d" /etc/hosts
+sudo sed -i "/$HADOOP_DN_ONE_IP $HADOOP_DN_ONE_NAME/d" /etc/hosts
+sudo sed -i "/$HADOOP_DN_TWO_IP $HADOOP_DN_TWO_NAME/d" /etc/hosts
+
+# Delete hadoop user
+sudo deluser --remove-home hadoop
+
+# Uninstall hadoop
+sudo rm -r /opt/hadoop/
+
+# ==========
+
+#REMOVE START HADOOP AT SYSTEM BOOT!!!!!!!!!!!!!!!!!
+#STOP HADOOP!!!!!!!!!!!!!!!!!!!!!!!!!!!
---
+++ datalayer/hadoop/UNINSTALL_HADOOP_SNN.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+HADOOP_NN_IP=$1
+HADOOP_NN_NAME=$2
+
+cd
+
+# Remove hadoop name node vm from hosts
+sudo sed -i "/$HADOOP_NN_IP $HADOOP_NN_NAME/d" /etc/hosts
+
+# Delete hadoop user
+sudo deluser --remove-home hadoop
+
+# Uninstall hadoop
+sudo rm -r /opt/hadoop/

18f7756286479a048bc958e40bcb2b192ad7d627
dilipts4u <31033201+dilipts4u@users.noreply.github.com>
2019-09-12 18:35:00 +0100

    Update uninstall_elk.sh

--- presentationlayer/ElasticSearch_Kibana_Logstash/scripts/uninstall_elk.sh
+++ presentationlayer/ElasticSearch_Kibana_Logstash/scripts/uninstall_elk.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+pkill -f elasticsearch
+pkill -f logstash
+pkill -f kibana
+
+ls -latr /opt/elk/
+ls -latr /home/dtpuser/
+
+sudo rm -rf /opt/elk
+sudo rm -rf /home/dtpuser/ELK
+ 
+
+ls -latr  /home/dtpuser
+ls -latr  /opt/

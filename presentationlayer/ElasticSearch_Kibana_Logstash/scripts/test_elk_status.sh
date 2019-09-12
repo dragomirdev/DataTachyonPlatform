7b77035ea913da488d7ec19e2a7be84c50098138
dilipts4u <31033201+dilipts4u@users.noreply.github.com>
2019-09-12 18:33:47 +0100

    Update test_elk_status.sh

--- presentationlayer/ElasticSearch_Kibana_Logstash/scripts/test_elk_status.sh
+++ presentationlayer/ElasticSearch_Kibana_Logstash/scripts/test_elk_status.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+######################### Testing Elastic-Search ###################################
+
+echo "Checking ElasticSearch "
+##curl -X GET "localhost:9200/"
+ip_address=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+curl -X GET http://${ip_address}:9200

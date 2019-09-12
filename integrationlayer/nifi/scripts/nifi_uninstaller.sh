615bebb185905ebe2db0a5332ab262c3ec5705d6
dilipts4u <31033201+dilipts4u@users.noreply.github.com>
2019-09-12 10:39:40 +0100

    Create nifi_uninstaller.sh

---
+++ integrationlayer/nifi/scripts/nifi_uninstaller.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euxo pipefail
+
+#USER_NAME='dtpuser'
+#echo $USER_NAME
+
+sudo -i -u dtpuser   bash << 'EOF'
+
+echo "Uninstalling Nifi"
+
+sudo /opt/nifi-1.9.2/bin/nifi.sh status
+sudo /opt/nifi-1.9.2/bin/nifi.sh stop
+
+rm -rf /opt/nifi-1.9.2-bin.zip
+rm -rf /opt/nifi-1.9.2
+rm /opt/nifi_remote_installation.sh
+
+ls -latr /home/dtpuser/
+
+EOF

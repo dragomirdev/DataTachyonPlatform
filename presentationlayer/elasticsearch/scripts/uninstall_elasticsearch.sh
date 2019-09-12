c3f79d5b81039b903d47fe3fdd24776711e0811a
dilipts4u <31033201+dilipts4u@users.noreply.github.com>
2019-09-12 16:33:17 +0100

    Update uninstall_elasticsearch.sh

--- presentationlayer/elasticsearch/scripts/uninstall_elasticsearch.sh
+++ presentationlayer/elasticsearch/scripts/uninstall_elasticsearch.sh
@@ -1,1 +1,18 @@
-  
+#!/bin/bash
+
+pkill -f elasticsearch
+pkill -f logstash
+pkill -f kibana
+
+ls -latr /opt/elk/
+ls -latr /home/dtpuser/
+
+sudo rm -rf /opt/elk
+sudo rm -rf /home/dtpuser/elasticsearch*
+sudo rm -rf /home/dtpuser/logstash*
+sudo rm -rf /home/dtpuser/kibana*
+sudo rm -rf /home/dtpuser/*.sh
+sudo 
+
+ls -latr  /home/dtpuser
+ls -latr  /opt/

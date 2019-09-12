041fa534329a46b2578c986a239444d1f567c70d
dilipts4u <31033201+dilipts4u@users.noreply.github.com>
2019-09-12 17:36:44 +0100

    Update install_elk.sh

--- presentationlayer/scripts/install_elk.sh
+++ presentationlayer/scripts/install_elk.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+cd ..
+
+echo "Initiating ElasticSearch"
+./presentationlayer/elasticsearch/scripts/install_elasticsearch.sh
+
+echo "Initiating ElasticSearch"
+./presentationlayer/kibana/scripts/install_kibana.sh

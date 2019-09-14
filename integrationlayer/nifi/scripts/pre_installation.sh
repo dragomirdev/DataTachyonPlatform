6bd9ad4bb3fca3ed24c245afa2cdcefa43a502d6
dilipts4u <31033201+dilipts4u@users.noreply.github.com>
2019-09-14 08:05:22 +0100

    Update pre_installation.sh

--- integrationlayer/nifi/scripts/pre_installation.sh
+++ integrationlayer/nifi/scripts/pre_installation.sh
@@ -1,1 +1,15 @@
+## Generate ssh keys
+ssh-keygen -t rsa
+<no password>
 
+## add the public keys to the authorized keys
+cat .ssh/id_rsa.pub >> .ssh/authorized_keys
+sudo chmod -R 700 .ssh
+sudo chmod -R 640 .ssh/authorized_keys
+
+## adding the localhost keys to the known_hosts
+ssh localhost
+
+## Copying the public keys of the remote host/VM to the local .ssh/authorized_keys file.
+Do ssh-copy-id:
+ssh-copy-id <USER>@<HOST_NAME>

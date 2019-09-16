e5a830ab623fcd27d47dcab052a2f64faeb214fb
dilipts4u <31033201+dilipts4u@users.noreply.github.com>
2019-09-16 11:46:22 +0100

    Update jenkins_installation.sh

--- cicd/jenkins/scripts/jenkins_installation.sh
+++ cicd/jenkins/scripts/jenkins_installation.sh
@@ -11,8 +11,8 @@ sudo apt install openjdk-8-jdk
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-sudo apt update\
-sudo apt install jenkins
+sudo apt update -y
+sudo apt install jenkins -y
 
 sudo systemctl enable jenkins
 sudo systemctl status jenkins

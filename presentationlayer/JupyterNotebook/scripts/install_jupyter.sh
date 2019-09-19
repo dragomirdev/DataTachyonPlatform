6c8ddce9651ccb87cb7521d0c320030055a4d346
dilipts4u <dilip.tsridhar@gmail.com>
2019-09-19 21:43:50 +0100

    Updated Presentation Layer

---
+++ presentationlayer/JupyterNotebook/scripts/install_jupyter.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+###Note##
+#Before running job ...
+#(1) Create the users dtpuser and spark on ELK VM
+#(2) Add exception to sudoers file for the users dtpuser
+#(3) Copy the public key of jenkins user on jenkins VM to dtpuser on ELK VM
+
+#set -euxo pipefail
+
+# Install dependecies
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install openjdk-8-jdk zip unzip net-tools ncdu inxi dstat iftop vnstat bmon nmap tcpdump nfs-common ack-grep zip unzip htop vim nano
+sudo apt -y autoremove
+
+python3 -V
+
+sudo apt -y install  python3-pip python3-dev
+sudo wget https://bootstrap.pypa.io/get-pip.py
+sudo python3.6 get-pip.py
+
+pip3 install --upgrade notebook
+
+# Login as dtpuser Run the following commands manually.
+jupyter notebook --generate-config
+
+# Manual one time step to store password
+# jupyter notebook password
+
+# Manual step Run Jupyter Notebook
+# jupyter notebook --ip=0.0.0.0 --no-browser
+
+
+
+

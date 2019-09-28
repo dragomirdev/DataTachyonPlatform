7eed42db9a9f381a614bb5f1d47044748b9e38ef
dilipts4u <dilip.tsridhar@gmail.com>
2019-09-28 15:53:55 +0100

    Updated output to push to hdfs

--- businesslayer/ai/deep-learning/tensorboard/scripts/run_tensor_board_model_fit.sh
+++ businesslayer/ai/deep-learning/tensorboard/scripts/run_tensor_board_model_fit.sh
@@ -6,15 +6,15 @@ pkill -f tensorboard
 pwd
 current_date=$(date '+%Y-%m-%d')
 
-echo "Running Tensorboard for Model Fit Type on" $current_date
-tensorboard --logdir logs/fit &
-
 TARGET_HDFS_PROCESSED_FOLDER=/data/dtp/processed/mnist/
 
-echo "Pushing Output logs to the HDFS Process Folder" $TARGET_HDFS_PROCESSED_FOLDER
+echo "Started Pushing Output logs to the HDFS Process Folder" $TARGET_HDFS_PROCESSED_FOLDER
 
 hdfs dfs -mkdir -p $TARGET_HDFS_PROCESSED_FOLDER
 hdfs dfs -put logs $TARGET_HDFS_PROCESSED_FOLDER
 
-
 echo "Completed Pushing Output logs to the HDFS Process Folder " $TARGET_HDFS_PROCESSED_FOLDER
+
+echo "Running Tensorboard for Model Fit Type on" $current_date
+tensorboard --logdir logs/fit &
+

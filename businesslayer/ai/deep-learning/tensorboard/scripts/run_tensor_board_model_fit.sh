550ff715169b47744b2abe1d0b0abd047f5a81dd
dilipts4u <dilip.tsridhar@gmail.com>
2019-09-28 11:45:27 +0100

    Added Tensorflow-TensorBoard Demo  scripts

--- businesslayer/ai/deep-learning/tensorboard/scripts/run_tensor_board_model_fit.sh
+++ businesslayer/ai/deep-learning/tensorboard/scripts/run_tensor_board_model_fit.sh
@@ -4,9 +4,6 @@ clear
 
 pwd
 
-# Clear any logs from previous runs
-rm -rf ./logs/
-
 echo "Running Tensorboard for Model Fit Type"
 tensorboard --logdir logs/fit &
 
--- businesslayer/ai/deep-learning/tensorboard/scripts/run_tensorboard_gradient_tape.sh
+++ businesslayer/ai/deep-learning/tensorboard/scripts/run_tensorboard_gradient_tape.sh
@@ -4,8 +4,5 @@ clear
 
 pwd
 
-# Clear any logs from previous runs
-rm -rf ./logs/
-
 echo "Running Tensorboard for the Gradient Type"
 tensorboard --logdir logs/gradient_tape &

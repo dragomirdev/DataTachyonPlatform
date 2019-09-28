728a2e4d22ddc2f82329298e44ba7ac3133d0f2a
dilipts4u <dilip.tsridhar@gmail.com>
2019-09-28 15:02:34 +0100

    Added the Output folder to be pushed to HDFS

--- businesslayer/ai/deep-learning/tensorboard/scripts/run_tensor_board_model_fit.sh
+++ businesslayer/ai/deep-learning/tensorboard/scripts/run_tensor_board_model_fit.sh
@@ -3,7 +3,15 @@
 clear
 
 pwd
+current_date=$(date '+%Y-%m-%d')
 
-echo "Running Tensorboard for Model Fit Type"
+echo "Running Tensorboard for Model Fit Type on" $current_date
 tensorboard --logdir logs/fit &
+
+TARGET_HDFS_PROCESSED_FOLDER=/data/dtp/processed/mnist/model-fit-type/ingestiondate=$current_date
+
+echo "Pushing to the HDFS Process Folder" $TARGET_HDFS_PROCESSED_FOLDER
+hdfs dfs -mkdir -p $TARGET_HDFS_PROCESSED_FOLDER
+hdfs dfs -put logs $TARGET_HDFS_PROCESSED_FOLDER
+
 
--- businesslayer/ai/deep-learning/tensorboard/scripts/run_tensorboard_gradient_tape.sh
+++ businesslayer/ai/deep-learning/tensorboard/scripts/run_tensorboard_gradient_tape.sh
@@ -6,3 +6,25 @@ pwd
 
 echo "Running Tensorboard for the Gradient Type"
 tensorboard --logdir logs/gradient_tape &
+
+hdfs dfs -mkdir -p /data/dtp/processed/mnist/gradient-type/
+hdfs dfs -put logs /data/dtp/processed/mnist/gradient-type/
+
+
+clear
+
+pwd
+current_date=$(date '+%Y-%m-%d')
+
+echo "Running Tensorboard for Gradient Type for Date: " $current_date
+tensorboard --logdir logs/gradient_tape &
+
+TARGET_HDFS_PROCESSED_FOLDER=/data/dtp/processed/mnist/gradient_tape/ingestiondate=$current_date
+
+echo "Pushing to the HDFS Process Folder" $TARGET_HDFS_PROCESSED_FOLDER
+
+hdfs dfs -mkdir -p $TARGET_HDFS_PROCESSED_FOLDER
+hdfs dfs -put logs $TARGET_HDFS_PROCESSED_FOLDER
+
+
+

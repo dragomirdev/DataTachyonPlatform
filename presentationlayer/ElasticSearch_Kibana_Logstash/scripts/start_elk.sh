1fd4f6d87464dd9d313a7dee1d0c30712d0a9814
dilipts4u <dilip.tsridhar@gmail.com>
2019-10-01 17:12:43 +0100

    Updated ELK Startup Services

--- presentationlayer/ElasticSearch_Kibana_Logstash/scripts/start_elk.sh
+++ presentationlayer/ElasticSearch_Kibana_Logstash/scripts/start_elk.sh
@@ -54,7 +54,7 @@ ps -ef | grep logstash
 
 echo " Starting LogStash Server...."
 #Start Logstash
-/opt/elk/logstash/bin/logstash -e 'input { stdin { } } output { stdout {} }' 
+/opt/elk/logstash/bin/logstash -e 'input { stdin { } } output { stdout {} }'
 
 #sudo  ./opt/elk/logstash-7.2.0/run_logstash.sh
 #LOGSTASH_LOG=/datadrive/elk/logstash/log/logstash.log

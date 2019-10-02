41078d6d9448764133e7611f8f28dd8286c1a2d5
dilipts4u <dilip.tsridhar@gmail.com>
2019-10-02 11:40:39 +0100

    Updated Liferay

--- presentationlayer/Liferay/scripts/start_portal.sh
+++ presentationlayer/Liferay/scripts/start_portal.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd /opt/liferay-portal-7.2.0-ga1/
-
 echo "Starting Liferay Portal Server"
-./tomcat-9.0.17/bin/startup.sh
+./opt/liferay-portal-7.2.0-ga1/tomcat-9.0.17/bin/startup.sh
 
 ps -ef | grep "liferay"

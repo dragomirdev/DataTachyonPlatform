a93f8985f45b81e5d64a363cae7587ad7ca39c31
dilipts4u <dilip.tsridhar@gmail.com>
2019-10-02 10:25:08 +0100

    Updated Nifi

--- integrationlayer/nifi/scripts/start_nifi.sh
+++ integrationlayer/nifi/scripts/start_nifi.sh
@@ -4,3 +4,6 @@ ps -ef | grep nifi
 echo "Starting Nifi"
 /opt/nifi-1.9.2/bin/nifi.sh start
 sleep 1m
+
+echo "Checking Nifi"
+/opt/nifi-1.9.2/bin/nifi.sh status
--- integrationlayer/nifi/scripts/stop_nifi.sh
+++ integrationlayer/nifi/scripts/stop_nifi.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+
+echo "Stopping Nifi"
+/opt/nifi-1.9.2/bin/nifi.sh stop
+
 echo "Checking Nifi"
 /opt/nifi-1.9.2/bin/nifi.sh status
+
+
 
 

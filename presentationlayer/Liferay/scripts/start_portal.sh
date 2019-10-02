d39d260d3c0a51a313b265d4e39a57ee874be049
dilipts4u <dilip.tsridhar@gmail.com>
2019-10-02 11:29:44 +0100

    Updated Liferay

--- presentationlayer/Liferay/scripts/start_portal.sh
+++ presentationlayer/Liferay/scripts/start_portal.sh
@@ -4,3 +4,5 @@ cd /opt/liferay-portal-7.2.0-ga1/
 
 echo "Starting Liferay Portal Server"
 ./tomcat-9.0.17/bin/startup.sh
+
+ps -ef | grep "liferay"
--- presentationlayer/Liferay/scripts/stop_portal.sh
+++ presentationlayer/Liferay/scripts/stop_portal.sh
@@ -7,9 +7,9 @@ ps -ef | pgrep -f "liferay" | xargs kill -9
 
 sudo systemctl stop liferay
 
-ps -ef | grep liferay
-
 cd /opt/liferay-portal-7.2.0-ga1/
 
 echo "Stopping Liferay Portal Server"
 ./tomcat-9.0.17/bin/stop.sh
+
+ps -ef | grep "liferay"

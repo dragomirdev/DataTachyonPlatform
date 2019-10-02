ce11add2d15c2dd4d7c8389d96d6709bbb7951f8
dilipts4u <dilip.tsridhar@gmail.com>
2019-10-02 11:48:42 +0100

    Updated Liferay

--- presentationlayer/Liferay/scripts/stop_portal.sh
+++ presentationlayer/Liferay/scripts/stop_portal.sh
@@ -8,6 +8,6 @@ ps -ef | pgrep -f "liferay" | xargs kill -9
 sudo systemctl stop liferay
 
 echo "Stopping Liferay Portal Server"
-./opt/liferay-portal-7.2.0-ga1/tomcat-9.0.17/bin/stop.sh
+./opt/liferay-portal-7.2.0-ga1/tomcat-9.0.17/bin/shutdown.sh
 
 ps -ef | grep "liferay"

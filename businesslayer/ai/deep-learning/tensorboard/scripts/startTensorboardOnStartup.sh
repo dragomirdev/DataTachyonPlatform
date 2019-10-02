9e935ab3108b2a15dba3577b6c757f204de639d4
dilipts4u <dilip.tsridhar@gmail.com>
2019-10-02 10:57:08 +0100

    Updated TensorBoard

--- businesslayer/ai/deep-learning/tensorboard/scripts/startTensorboardOnStartup.sh
+++ businesslayer/ai/deep-learning/tensorboard/scripts/startTensorboardOnStartup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 /usr/bin/python3.6 /usr/local/bin/tensorboard --logdir  /opt/DataTachyonPlatform/businesslayer/ai/deep-learning/tensorboard/scripts/logs/gradient_tape &
+
+ps -ef | grep tensorboard

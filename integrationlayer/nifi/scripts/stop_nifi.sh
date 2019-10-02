b4ad5f51a2afc78e4b9d78b3c6ed46a3575ce37e
dilipts4u <dilip.tsridhar@gmail.com>
2019-10-02 10:40:11 +0100

    Updated Nifi

--- integrationlayer/nifi/scripts/stop_nifi.sh
+++ integrationlayer/nifi/scripts/stop_nifi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo systemctl stop nifi
 
 echo "Stopping Nifi"
 /opt/nifi-1.9.2/bin/nifi.sh stop
--- presentationlayer/ElasticSearch_Kibana_Logstash/scripts/stop_elk.sh
+++ presentationlayer/ElasticSearch_Kibana_Logstash/scripts/stop_elk.sh
@@ -5,6 +5,13 @@ pkill -f elasticsearch
 pkill -f logstash
 pkill -f kibana
 
+sudo systemctl stop logstash
+sudo systemctl stop kibana
+sudo systemctl stop elasticsearch
+
+ps -ef | pgrep -f "logstash" | xargs kill -9
+ps -ef | pgrep -f "kibana" | xargs kill -9
+ps -ef | pgrep -f "elasticsearch" | xargs kill -9
 
 ps -ef | grep elasticsearch
 ps -ef | grep kibana

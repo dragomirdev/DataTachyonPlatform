6203cf6b5d03ba726d7968bfa199dbfff5c7359b
dilipts4u <dilip.tsridhar@gmail.com>
2019-10-10 17:17:39 +0100

    Changed the Tensorboard port to 8008

--- businesslayer/ai/deep-learning/tensorboard/scripts/startTensorboardOnStartup.sh
+++ businesslayer/ai/deep-learning/tensorboard/scripts/startTensorboardOnStartup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/bin/python3.6 /usr/local/bin/tensorboard --logdir --port=8008 /opt/DataTachyonPlatform/businesslayer/ai/deep-learning/tensorboard/scripts/logs/gradient_tape &
+/usr/bin/python3.6 /usr/local/bin/tensorboard --logdir  /opt/DataTachyonPlatform/businesslayer/ai/deep-learning/tensorboard/scripts/logs/gradient_tape  --port=8008  &
 
 ps -ef | grep tensorboard

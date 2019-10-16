cc606ef42b1a7d1d6ba4505165249dc811877e60
dilipts4u <dilip.tsridhar@gmail.com>
2019-10-16 19:28:48 +0100

    Adding Python Kafka Demo

--- integrationlayer/kafka/scripts/test/KafkaProducer.py
+++ integrationlayer/kafka/scripts/test/KafkaProducer.py
@@ -1,7 +1,7 @@
 #==========================================================
 # Load libraries and constants
 #==========================================================
-
+import random
 from time import sleep
 from json import dumps
 from kafka import KafkaProducer
@@ -28,26 +28,33 @@ def main(args):
     producer = KafkaProducer(bootstrap_servers=[kafka_listener],
                              value_serializer=lambda x: dumps(x).encode('utf-8'))
 
-    class SensorInfo:
-        def __init__(self, id, name, temperature, pressure,
-                     humidity, latitude, longitude):
-            self.id = id
-            self.name = name
-            self.temperature = temperature
-            self.pressure = pressure
-            self.humidity = humidity
-            self.latitude = latitude
-            self.longitude = longitude
-
     print('Sending Messages to Kafka Topic: ' + kafka_topic_name)
+    start = 0
+    stop = 100
 
     for e in range(5):
-        payload = SensorInfo(e, 'MachineSensor123', 30, 100, 20, 22.1, 25.1)
+        payload = SensorInfo(e, 'MachineSensor123', getRandomFloat(start, stop), 100, 20, 22.1, 25.1)
         payload_data = dumps(payload.__dict__)
         print(payload_data)
         #data = {'ID': e}
         producer.send(kafka_topic_name, value=payload_data)
         sleep(5)
+
+
+class SensorInfo:
+    def __init__(self, id, name, temperature, pressure,
+                 humidity, latitude, longitude):
+        self.id = id
+        self.name = name
+        self.temperature = temperature
+        self.pressure = pressure
+        self.humidity = humidity
+        self.latitude = latitude
+        self.longitude = longitude
+
+
+def getRandomFloat(start, stop):
+    return random.uniform(start, stop)
 
 
 if __name__ == '__main__':

513782507c709b866f46eb8da2bc4373aaa7cccc
dilipts4u <dilip.tsridhar@gmail.com>
2019-10-19 16:38:14 +0100

    Added Dynamic Json Msg

--- integrationlayer/kafka/scripts/test/KafkaProducer.py
+++ integrationlayer/kafka/scripts/test/KafkaProducer.py
@@ -7,6 +7,10 @@ from json import dumps
 from kafka import KafkaProducer
 import sys
 import uuid
+import datetime
+from json import JSONDecoder
+from json import JSONEncoder
+
 
 start = 0
 stop = 100
@@ -17,16 +21,62 @@ def getRandomFloat(start, stop):
     return round(random.uniform(start, stop), noOfDecimalPlaces)
 
 
-def getSensorPayload(id, name, temperature, pressure, humidity, latitude, longitude):
+def getSensorPayload(id, name, temperature, pressure, humidity, event_datetime, latitude, longitude):
     payload = '{"id": ' + str(id) + ', ' \
               '"name": "' + str(name) + '", ' \
               '"temperature": ' + str(temperature) + ', ' \
               '"pressure": ' + str(pressure) + ', ' \
               '"humidity": ' + str(humidity) + ', ' \
+              '"event_datetime": ' + str(event_datetime) + ', ' \
               '"latitude": ' + str(latitude) + ', ' \
               '"longitude": ' + str(longitude) + '}'
-    # print("payload\n", payload)
+    #print("payload\n", payload)
     return payload
+
+###
+
+class DateTimeDecoder(json.JSONDecoder):
+
+    def __init__(self, *args, **kargs):
+        JSONDecoder.__init__(self, object_hook=self.dict_to_object,
+                             *args, **kargs)
+
+    def dict_to_object(self, d):
+        if '__type__' not in d:
+            return d
+
+        type = d.pop('__type__')
+        try:
+            dateobj = datetime(**d)
+            return dateobj
+        except:
+            d['__type__'] = type
+            return d
+
+
+
+class DateTimeEncoder(JSONEncoder):
+    """ Instead of letting the default encoder convert datetime to string,
+        convert datetime objects into a dict, which can be decoded by the
+        DateTimeDecoder
+    """
+
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return {
+                '__type__': 'datetime',
+                'year': obj.year,
+                'month': obj.month,
+                'day': obj.day,
+                'hour': obj.hour,
+                'minute': obj.minute,
+                'second': obj.second,
+                'microsecond': obj.microsecond,
+            }
+        else:
+            return JSONEncoder.default(self, obj)
+
+####
 
 
 def getKafkaProducer(kafka_listener):
@@ -51,9 +101,12 @@ def sendMessages(args):
         humidity = getRandomFloat(start, 50)
         latitude = getRandomFloat(start, stop)
         longitude = getRandomFloat(start, stop)
-        payload = getSensorPayload(id, name, temperature, pressure, humidity, latitude, longitude)
+        event_datetime = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+        print(event_datetime)
+        payload = getSensorPayload(id, name, temperature, pressure, humidity, event_datetime, latitude, longitude)
         # Decoding or converting JSON format in dictionary using loads()
-        dict_obj = json.loads(payload)
+        # dict_obj = json.loads(payload)
+        dict_obj = json.loads(json.dumps(payload, cls=DateTimeEncoder), cls=DateTimeDecoder)
         print("payload", dict_obj)
         print('Sending Messages to Kafka Topic: ' + kafka_topic_name)
         producer.send(kafka_topic_name, value=dict_obj)

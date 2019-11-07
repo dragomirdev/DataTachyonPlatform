c239db984c2443fdb0d1a2d027c4d819e264427c
Dilip Thallam Sridhar <dilip.tsridhar@gmail.com>
2019-11-07 12:31:19 +0000

    Added vanilla scripts

--- integrationlayer/kafka/scripts/test/vanilla/KafkaProducer.py
+++ integrationlayer/kafka/scripts/test/vanilla/KafkaProducer.py
@@ -1,7 +1,6 @@
 #==========================================================
 # Load libraries and constants
 #==========================================================
-
 import json
 import random
 from json import dumps
@@ -65,5 +64,55 @@ class DateTimeDecoder(json.JSONDecoder):
             return d
 
 
+class DateTimeEncoder(JSONEncoder):
+    """ Instead of letting the default encoder convert datetime to string,
+        convert datetime objects into a dict, which can be decoded by the
+        DateTimeDecoder
+    """
 
-                                                                                                                                                                           1
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        else:
+            return JSONEncoder.default(self, obj)
+
+####
+
+
+def getKafkaProducer(kafka_listener):
+    producer = KafkaProducer(bootstrap_servers=[kafka_listener],
+                             value_serializer=lambda x: dumps(x, cls=DateTimeEncoder).encode('utf-8'))
+    #json.loads(json.dumps(payload, cls=DateTimeEncoder), cls=DateTimeDecoder)
+    return producer
+
+
+def sendMessages(args):
+    kafka_listener = args[1]
+    kafka_topic_name = args[2]
+    kafka_listener = 'JP-DTP-KAFKA-VM:9092'
+    kafka_topic_name = 'DTPTopic'
+    producer = getKafkaProducer(kafka_listener)
+
+    for e in range(5):
+        id = str(uuid.uuid1().int)[:10]
+        name = "MachineSensor"
+        temperature = getRandomFloat(start, stop)
+        pressure = getRandomFloat(start, stop)
+        humidity = getRandomFloat(start, 50)
+        event_date = getRandomDateInCurrentYear()
+        latitude = getRandomFloat(start, stop)
+        longitude = getRandomFloat(start, stop)
+        event_datetime = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+        print(event_datetime)
+        payload = getSensorPayload(id, name, temperature, pressure, humidity, event_date, event_datetime, latitude, longitude)
+        # Decoding or converting JSON format in dictionary using loads()
+        dict_obj = json.loads(payload)
+        #dict_obj = json.loads(json.dumps(payload, cls=DateTimeEncoder), cls=DateTimeDecoder)
+        print("payload", dict_obj)
+        print('Sending Messages to Kafka Topic: ' + kafka_topic_name)
+        producer.send(kafka_topic_name, value=dict_obj)
+        producer.flush()
+
+
+if __name__ == '__main__':
+    sendMessages(sys.argv)

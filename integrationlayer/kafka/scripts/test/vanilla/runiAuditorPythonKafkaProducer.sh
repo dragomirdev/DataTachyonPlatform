778188810bf2802ba30a3456f6aef44f9079976a
Dilip Thallam Sridhar <dilip.tsridhar@gmail.com>
2019-11-12 11:07:39 +0000

    Added iAudit Kafka Producer

---
+++ integrationlayer/kafka/scripts/test/vanilla/runiAuditorPythonKafkaProducer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+clear
+
+kafka_listener='JP-DTP-KAFKA-VM:9092'
+kafka_topic_name='DTPTopic'
+
+echo "Starting Kafka Producer"
+python3 KafkaProducer.py kafka_listener kafka_topic_name

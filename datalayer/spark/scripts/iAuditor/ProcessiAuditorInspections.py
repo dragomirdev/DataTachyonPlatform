b852a4df137cc2bd166c7758517f1ea506edddfe
Dilip Thallam Sridhar <dilip.tsridhar@gmail.com>
2019-11-14 15:04:18 +0000

    Changed to csv

--- datalayer/spark/scripts/iAuditor/ProcessiAuditorInspections.py
+++ datalayer/spark/scripts/iAuditor/ProcessiAuditorInspections.py
@@ -88,7 +88,7 @@ def processiAuditorInspectionReport(args):
     #last_item_df = splitted_items_df.select(splitted_items_df.arr[size(splitted_items_df.arr) - 1]).show()
 
     #last_item_df = items_df.withColumn("lastItem", F.last(items_df.items)).drop(items_df.items)
-    last_item_df = items_df.withColumn("lastItem", items_df.items(size(items_df.items)-1)).drop(items_df.items)
+    last_item_df = items_df.withColumn("lastItem", col("items")(size(col("items"))-1)).drop(items_df.items)
     last_item_df.printSchema()
     last_item_df.show()
 

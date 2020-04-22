e33105e40830dc7dd6d8777b323ebecb3ba2ad73
dragomirdev <dragomir.kovacevic@gmail.com>
2020-04-22 16:13:57 +0100

    Add files via upload

---
+++ common/scripts/dtp/spark/startSpark.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ~/.bash_profile
+echo "Starting Spark"
+
+cd /opt/spark
+
+/opt/spark/sbin/start-all.sh
+
---
+++ common/scripts/dtp/spark/stopSpark.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source ~/.bash_profile
+echo "Stopping Spark"
+cd /opt/dtp
+/opt/spark/sbin/stop-all.sh
+
+
